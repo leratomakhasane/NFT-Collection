123f29da8532db3b1dba63888e72070c20ad8668
Lerato Rampai <87371242+leratomakhasane@users.noreply.github.com>
2022-07-11 22:26:23 +0200

    updated nft contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -2,6 +2,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x56cf4D4A59FD0b626A528c7872BEbDe783e39793";
 
 //URL to extract metadata for Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-leratomakhasane.vercel.app/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
